e298f84ed82ba88be8ee031f497cb784c99ae3a2
Landon Lapensee <landonlapensee@gmail.com>
2021-02-23 09:12:21 -0800

    Changed db and files relating to name error when we created the project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "CHANGE_THIS_TO_YOUR_RAILS_APP_NAME_production"
+  # config.active_job.queue_name_prefix = "gearify_production"
 
   config.action_mailer.perform_caching = false
 
