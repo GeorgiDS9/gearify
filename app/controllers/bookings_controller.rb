f935ec69c93c357ed50019737c96fcc87fc01f77
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-02-24 12:40:39 -0500

    Added the show and index routes for bookings. Added the show method in bookings controller, added booking params permissions in bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,12 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :user_id, :gear_id)
+  end
+
 end
--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -40,30 +40,3 @@ class GearsController < ApplicationController
     params.require(:gear).permit(:description, :price, :category, :name)
   end
 end
-
-# def index
-#   @cocktails = Cocktail.all
-# end 
-
-# def show
-#   @cocktail = Cocktail.find(params[:id])
-#   # @dose = Dose.find(params[:dose_id])
-#   @ingredients = Ingredient.all
-# end 
-
-# def new
-#   @cocktail = Cocktail.new 
-# end
-
-# def create
-#   cocktail = Cocktail.new(cocktail_params)
-#   cocktail.save
-
-#   redirect_to cocktails_path
-# end
-
-# private
-
-# def cocktail_params
-#   params.require(:cocktail).permit(:name)
-# end 
