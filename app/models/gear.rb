2925a5d36b2ea728da49536ffe61f2deb49f38f6
Nicolas Tsu <nicolas.tsu@hec.ca>
2021-02-24 15:07:06 -0500

    added content for gears views, link to navigate and some styling

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -2,4 +2,6 @@ class Gear < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
   validates :price, :description, :category, :name, presence: true;
+
+  CATEGORIES = ["biking", "hiking", "scuba", "skiing"]
 end
