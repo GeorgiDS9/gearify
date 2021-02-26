5c348947a199e9ad852dda242cbca40e59f8e2b2
Nicolas Tsu <nicolas.tsu@hec.ca>
2021-02-26 14:00:11 -0500

    added edit/update actions to gears controller and edit page to gears

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :gears, only: [ :index, :show, :new, :create, :destroy ] do
+  resources :gears do
     resources :bookings, only: [ :new, :create ]
   end
   get 'users/profile', to: 'users#profile', as: 'profile'
